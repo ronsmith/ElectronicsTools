1b79f4721a221ea537d6b27f7e90a5f68159e533
Ron Smith <ron.smith@thataintworking.com>
2023-12-14 17:14:02 -0500

    Switched to using servo library

--- ServoDriver328P/ServoDriver328P.ino
+++ ServoDriver328P/ServoDriver328P.ino
@@ -1,7 +1,11 @@
-
+#include <Servo.h>
 #include "LED4D7SDisplay.h"
 #include "ClickLowButton.h"
 
+#define SERVO_PIN A0
+#define CHANGE_INC 100
+#define MIN_US 500
+#define MAX_US 2500
 
 const byte digit_pins[] = {A4, 11, 12, 13};
 const byte segment_pins[] = {2, 3, 4, 5, 6, 7, 8, A5};
@@ -11,12 +15,9 @@ LED4D7SDisplay display(digit_pins, segment_pins);
 ClickLowButton up_button(10);
 ClickLowButton down_button(9);
 
-#define PPM_PIN A0
-#define CHANGE_INC 100
-#define MIN_DUR 0
-#define MAX_DUR 9900
+Servo servo;
 
-unsigned int pulse_duration = 0;
+unsigned int us = 0;
 unsigned long end_pulse = 0;
 unsigned long end_period = 0;
 
@@ -26,12 +27,33 @@ void setup() {
   display.setup();
   up_button.setup();
   down_button.setup();
+  servo.attach(SERVO_PIN);
+  
+  servo.write(0);
+  delay(500);
+  Serial.print("0 degrees is "); Serial.print(servo.readMicroseconds()); Serial.println("μs");
+  servo.write(90);
+  delay(500);
+  Serial.print("90 degrees is "); Serial.print(servo.readMicroseconds()); Serial.println("μs");
+  servo.write(180);
+  delay(500);
+  Serial.print("180 degrees is "); Serial.print(servo.readMicroseconds()); Serial.println("μs");
+
+  servo.detach();
+  delay(500);
+  servo.attach(SERVO_PIN);
 }
 
 void loop() {
-  pulse_period();
+  if (us > 0) {
+    servo.writeMicroseconds(us);
+  }
   if (handle_buttons()) {
-    display.update(pulse_duration);
+    if (us > 0) {
+      display.update(us);
+    } else {
+      display.clear();
+    }
   } else {
     display.refresh();
   }
@@ -43,42 +65,37 @@ bool handle_buttons() {
   bool ubp = up_button.isPressed();
 
   if (dbp && ubp) { // reset to zero when both buttons pressed
-    pulse_duration = MIN_DUR;
+    Serial.println("Both buttons pressed -- reset");
+    us = 0;
     changed = true;
   } 
-  else if (dbp && pulse_duration > MIN_DUR) {
-    pulse_duration -= CHANGE_INC;
-    changed = true;
+  else if (dbp) {
+    Serial.println("Down button pressed");
+    if (us > MIN_US) {      
+      us -= CHANGE_INC;
+      changed = true;
+    } 
+    else if (us > 0) {
+      us = 0;
+      changed = true;
+    }
   } 
-  else if (ubp && pulse_duration < MAX_DUR) {
-    pulse_duration + CHANGE_INC;
-    changed = true;
+  else if (ubp) {
+    Serial.println("Up button pressed");
+    if (us < MAX_US) {
+      if (us > 0) {
+        us += CHANGE_INC;
+      } else {
+        us = MIN_US;
+      }
+      changed = true;
+    }
   }
-
+  
+  if (changed) {
+    Serial.print("Changed us to ");
+    Serial.print(us);
+    Serial.println("μs");
+  }
   return changed;
 }
-
-void pulse_period() {
-  if (pulse_duration > 0) {
-    if (micros() > end_pulse) {
-      digitalWrite(PPM_PIN, LOW);
-    }
-    if (millis() > end_period) {
-      begin_new_period();
-    }
-  } 
-  else {
-    digitalWrite(PPM_PIN, LOW);
-  }
-}
-
-void begin_new_period() {
-  if (pulse_duration > 0) {
-    end_pulse = micros() + pulse_duration;
-    digitalWrite(PPM_PIN, HIGH);
-  } else {
-    digitalWrite(PPM_PIN, LOW);
-  }
-  end_period = millis() + 20;
-}
-
